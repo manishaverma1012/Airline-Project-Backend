79f7796d7f10ccd9b4d3bb2b77ed18db6e0823bd
manishaverma1012 <manishaverma140698@gmail.com>
2021-08-16 18:03:16 +0530

    .

--- src/main/java/com/lti/mypack/ProjectGladiatorApplication.java
+++ src/main/java/com/lti/mypack/ProjectGladiatorApplication.java
@@ -2,6 +2,11 @@ package com.lti.mypack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 
 @SpringBootApplication
 public class ProjectGladiatorApplication {
@@ -9,5 +14,18 @@ public class ProjectGladiatorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectGladiatorApplication.class, args);
 	}
-
+	@SuppressWarnings("deprecation")
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+          return new WebMvcConfigurerAdapter() {
+        	  @Override
+              public void addCorsMappings(CorsRegistry registry) {
+                 registry.addMapping("/rest/api/userdetails").allowedOrigins("http://localhost:4200");//look for this error
+                 
+              }
+        	  
+		}; 
+             
+          
+       }
 }
